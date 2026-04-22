24b16b133b80afdc380b3a183c57823eca31ce39
dacdoan <dacdoan@users.noreply.github.com>
2026-04-22 09:34:34 +0700

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -121,7 +121,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#2d582d", /* default background colour */
 };
 
 
